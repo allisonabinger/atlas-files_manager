5a9c65655d6e8d286b99ec2bc804d052af58a92f
Allison <allisonabinger@gmail.com>
2024-07-12 13:24:39 -0500

    A - 07/12 01:24 PM - redis constructed but not working properly, working on resolution

--- utils/redis.js
+++ utils/redis.js
@@ -1,3 +1,55 @@
 // Contains the class RedisClient to manage Redis server connections and operations
+import redis from 'redis';
+import { promisify } from 'util';
 
-class RedisClient
+
+class RedisClient {
+    // constructor creates client and declare async binds
+    constructor() {
+        this.client = redis.createClient();
+
+        this.client.on('connect', () => {
+            console.log('Redis client connected');
+        })
+
+        this.client.on('error', (err) => {
+            console.error(`Redis Client Error: ${err}`);
+            this.connected = false;
+        });
+
+        this.client.on('ready', () => {
+            console.log('Redis client is ready to use');
+            this.connected = true;
+        });
+
+        this.client.on('end', () => {
+            console.log('Redis client disconnected');
+            this.connected = false;
+          });
+
+        // binds promisified version of get, set, and del
+        this.getAsync = promisify(this.client.get).bind(this.client);
+        this.setAsync = promisify(this.client.set).bind(this.client);
+        this.delAsync = promisify(this.client.del).bind(this.client);
+    }
+    
+    isAlive() {
+        // returns true when connected, false if not
+        return this.client.connected;
+    }
+
+    async get(key) {
+        return await this.getAsync(key);
+    }
+
+    async set(key, value, duration) {
+        await this.setAsync(key, value, duration);
+    }
+
+    async del(key) {
+        await this.delAsync(key);
+    }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
