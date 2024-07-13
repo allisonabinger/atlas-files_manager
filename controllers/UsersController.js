252de283c8f4067ef8de76a3f33d3705fa6f4b27
dom <6157@holbertonstudents.com>
2024-07-13 15:53:36 -0500

    created new user class

---
+++ controllers/UsersController.js
@@ -0,0 +1,36 @@
+const sha1 = require('sha1');
+const dbClient = require('../utils/db');
+
+
+class UsersController {
+
+    //handles creation of new user
+    static async postNew(req, res) {
+        const { email, password } = req.body;
+
+        // validates the email
+        if (!email) {
+            return res.status(400).json({ error: 'Missing email' });
+        }
+
+        if (!password) {
+            return res.status(400).json({ error: 'Missing password' });
+        }
+
+        const foundUser = await dbClient.getUserByEmail(email);
+        if (foundUser) {
+            return res.status(400).json({ error: 'Already exist' });
+        }
+
+        //Hashing the password
+        const hashedPassword = sha1(password);
+
+        //Saves user to the database
+        const newUser = await dbClient.createUser(email, hashedPassword);
+
+        //Returns new user email and id
+        return res.status(201).json({ email: newUser.email, id: newUser.id });
+    }
+}
+
+module.exports = UsersController;
--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@
 const express = require('express');
 const router = express.Router();
 const { getStatus, getStats } = require('../controllers/AppController');
+const UserController = require('../controllers/UsersController');
 
 // DB and Redis Checks
 router.get('/status', getStatus);
 router.get('/stats', getStats);
+router.post('/users', UserController.postNew);
 
 module.exports = router;
