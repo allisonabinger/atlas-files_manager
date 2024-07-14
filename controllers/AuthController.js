41cc5389c6844f2db8f4c51d2cd4415aef3b9bf0
dom <6157@holbertonstudents.com>
2024-07-14 10:48:24 -0500

    added endpoint, getMe method to userController, getDisconnext method to AuthController

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -5,36 +5,67 @@ const dbClient = require('../utils/db');
 
 class AuthController {
 
-    // 
+    // signs in a user and generates a token
     static async getConnect(req, res) {
         const authHeader = req.headers.authorization;
 
+        // This line checks if Authorization header is present and begins with Basic
         if(!authHeader || !authHeader.startsWith('Basic ')) {
             return res.status(401).json({ error: 'Unauthorized' });
         }
 
+        // decodes the credentialss
         const encodedCredentials = authHeader.split(' ')[1];
-
         const decodedCredentials = buffer.from(encodedCredentials, 'base64').toString('ascii');
-
         const [decodedEmail, decodedPassword] = decodedCredentials.split(':');
 
+        // Hashes the password
         const hashedPassword = sha1(decodedPassword);
 
+        // finds database user by email
         const user = await dbClient.getUserEmail(decodedEmail);
 
+        // checks if user exists and password
         if(!user || user.password !== hashedPassword) {
             return res.status(401).json({ error: 'Unauthorized' });
         }
 
+        // generating random string
         const token = uuidv4();
         const key = `auth_${token}`;
 
-        //
+        //stores the key to redis with expiration
         await redisClient.set(key, user._id.toString(), 86400);//seconds in a day
 
-        return res.status(200).json({ token });
+        // Returns random generted token to the client
+        return res.status(200).json({ token }); // 
     }
 
-    //
+    // this method signs out the user
+    static async getDiconnect(req, res) {
+        // retrieves token from request headers
+        const token = req.header['x-token'];
+
+        // checks for token
+        if (!token) {
+          return res.status(401).json({ error: 'Unauthorized' });
+        }
+
+        // creates redis key using the token
+        const key =  `auth_${token}`;
+        // gets user id using the token from redis
+        const userId = await redisClient.get(key);
+
+        // checks if user id exists
+        if (!userId) {
+          return res.status(401).json({ error: 'Unauthorized' });
+        }
+
+        // deletes the token from redis
+        await redisClient.del(key);
+        // returns no content response
+        return res.status(204).send();
+    }      
 }
+
+module.exports = AuthController;
--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -17,19 +17,50 @@ class UsersController {
             return res.status(400).json({ error: 'Missing password' });
         }
 
-        const foundUser = await dbClient.getUserByEmail(email);
+        const foundUser = await dbClient.findUserByEmail(email);
         if (foundUser) {
             return res.status(400).json({ error: 'Already exist' });
         }
 
         //Hashing the password
-        const hashedPassword = sha1(password);
+        const hashedPassword = sha1(password);//produces a 160-bit (20-byte) hash value
 
         //Saves user to the database
         const result = await dbClient.createUser(email, hashedPassword);
 
         //Returns new user email and id
         return res.status(201).json({ email: result.ops[0].email, id: newUser.insertedId });
+    }
+
+    static async getMe(req, res) {
+        // retrieves token from request headers
+        const token = req.headers['x-token'];
+
+        // Checks for token
+        if (!token) {
+            return res.status(401).json({ error: 'Unauthorized' });
+        }
+
+        // grabs redis key using token
+        const key = `auth_${token}`;
+        // gets the user id linked to the redis token
+        const userId = await dbClient.get(key);
+
+        // checks for user id
+        if (!userId) {
+            return res.status(401).json({ error: 'Unauthorized' });
+        }
+
+        // obtains user info from database using user id
+        const user = await dbClient.findUserById(userId);
+
+        // checks if user exists
+        if (!user) {
+            return res.status(401).json({ error: 'Unauthorized' });
+        }
+
+        // returns the user email and id
+        return res.status(200).json({ id: user._id, email: user.email });
     }
 }
 
