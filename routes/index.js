56a1974208c6e0ac1def13d7591ec7d710db1407
Allison <allisonabinger@gmail.com>
2024-07-18 16:04:20 -0500

    A - 07/18 04:04 PM - Added documentation to index.js

--- routes/index.js
+++ routes/index.js
@@ -10,17 +10,27 @@ const FilesController = require('../controllers/FilesController');
 // DB and Redis Checks
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+
+// Creates a new User
 router.post('/users', UserController.postNew);
 // router.post('/users', (req, res) => {
 //     res.json({ message: 'User creatino endpoint is working' })
 // })
+
+// Gets Authentication token for 24 Hours
 router.get('/connect', AuthController.getConnect);
+// Removes Authentication token
 router.get('/disconnect', AuthController.getDisconnect);
+
+// Gets id and email for user
 router.get('/users/me', UserController.getMe);
+
+// Post and Get new File information based on file id or parent id
 router.post('/files', FilesController.postUpload);
 router.get('/files/:id', FilesController.getShow);
 router.get('/files', FilesController.getIndex);
 
+// update public attribute for files
 router.put('/files/:id/publish', FilesController.putPublish);
 router.put('/files/:id/unpublish', FilesController.putUnpublish);
 
